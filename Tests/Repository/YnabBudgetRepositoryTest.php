c00f80b2c7ab403e4e03f032babed079261bd507
Petrica Martinescu <pmartinescu@tremend.ro>
2017-03-29 00:28:23 +0300

    Removed old client

--- Tests/Repository/YnabBudgetRepositoryTest.php
+++ Tests/Repository/YnabBudgetRepositoryTest.php
@@ -4,7 +4,15 @@ namespace Petrica\Ynab\Tests;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class YnabBudgetRepositoryTest
+ *
+ * @package Petrica\Ynab\Tests
+ */
 class YnabBudgetRepositoryTest extends TestCase
 {
+    public function read()
+    {
 
+    }
 }
